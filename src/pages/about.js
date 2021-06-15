4d43e88d188376c74fbd5eba59ac7def9c8f34e2
hananalkalay <hananalkalay@gmail.com>
2021-06-16 01:04:03 +0300

    love

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+// Step 1: Import your component
+import * as React from 'react'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
